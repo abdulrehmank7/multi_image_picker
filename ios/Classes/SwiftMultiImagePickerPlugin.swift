df5b17d60d6b5934f747d6c5ba076095e9d91bd7
Abdul Rehman <57043846+abdulrehmank7@users.noreply.github.com>
2021-05-03 15:38:53 +0530

    Update SwiftMultiImagePickerPlugin.swift
    Removed cancel toast info.

--- ios/Classes/SwiftMultiImagePickerPlugin.swift
+++ ios/Classes/SwiftMultiImagePickerPlugin.swift
@@ -154,8 +154,6 @@ public class SwiftMultiImagePickerPlugin: NSObject, FlutterPlugin {
                     }
                 }, deselect: { (asset: PHAsset) -> Void in
                     totalImagesSelected -= 1
-                }, cancel: { (assets: [PHAsset]) -> Void in
-                    result(FlutterError(code: "CANCELLED", message: "The user has cancelled the selection", details: nil))
                 }, finish: { (assets: [PHAsset]) -> Void in
                     var results = [NSDictionary]();
                     for asset in assets {
